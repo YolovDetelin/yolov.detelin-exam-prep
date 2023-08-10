4fa8043e3cdde5d8819304fdfae00fce90fb6ce3
yolov.detelin <yolov.detelin@students.softuni.bg>
2023-08-10 20:13:21 +0300

    Change URL

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
-  await page.goto('https://yolov-detelin.onrender.com');
+  await page.goto('http://yolov-detelin.onrender.com');
   const heading = await page.$('h1');
   const text = await heading.textContent();
   expect(text).toBe('About');
--- tests/ui/add-movie.test.js
+++ tests/ui/add-movie.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add movie page', async ({ page }) => {
-    await page.goto('https://yolov-detelin.onrender.com/add-movie');
+    await page.goto('http://yolov-detelin.onrender.com/add-movie');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
